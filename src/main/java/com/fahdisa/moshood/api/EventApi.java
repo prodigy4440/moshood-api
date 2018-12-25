a17cabfce4d36b5f50d77eb499df122d048b8bc7
Isau Opeyemi AZEEZ <prodigy4442@gmail.com>
2018-12-25 14:41:22 +0100

    Event Service implemented

--- src/main/java/com/fahdisa/moshood/api/EventApi.java
+++ src/main/java/com/fahdisa/moshood/api/EventApi.java
@@ -1,8 +1,11 @@
 package com.fahdisa.moshood.api;
 
 import com.fahdisa.moshood.entity.Event;
+import com.fahdisa.moshood.service.EventService;
 import io.swagger.annotations.Api;
+import org.wildfly.swarm.spi.runtime.annotations.Post;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,10 +16,37 @@ import javax.ws.rs.core.Response;
 @Api(value = "Event", description = "Event API")
 public class EventApi {
 
+    @Inject
+    private EventService eventService;
+
+    @Post
+    public Response create(Event event){
+        return Response.ok(eventService.createEvent(event)).build();
+    }
+
+    @PUT
+    public Response update(Event event){
+        return Response.ok(eventService.updateEvent(event)).build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response removeEvent(@PathParam("id")Long id){
+        return Response.ok(eventService.removeEvent(id)).build();
+    }
+
     @Path("{id}")
     @GET
     public Response getEvent(@PathParam("id")Long id){
-        return Response.ok(new Event()).build();
+        return Response.ok(eventService.getEvent(id)).build();
+    }
+
+    @Path("all")
+    @GET
+    public Response getEvents(@QueryParam("id")Long id,
+                              @QueryParam("page")Integer page,
+                              @QueryParam("size")Integer size){
+        return Response.ok(eventService.getEvents(id, page, size)).build();
     }
 
 }
